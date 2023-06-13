622feb3ea91a07e6b3aadba1db296b7b143e6adb
Dominik Lanzerstorfer <dominik.lanzerstorfer@student.onslow.school.nz>
2023-06-13 12:07:40 +1200

    Created 3 main menu layouts

---
+++ HistoryRPG/main_menu.gd
@@ -0,0 +1,23 @@
+extends GridContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_play_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	pass # Replace with function body.
